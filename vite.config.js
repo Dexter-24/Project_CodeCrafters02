d5dd585a2f7420b2f2ba914c03a3267d950eec82
Denis Chernyak <chernyak.ds@gmail.com>
2024-07-30 20:35:28 +0300

    fonts added through vite plugin

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
+import webfontDownload from 'vite-plugin-webfont-dl';
 
 export default defineConfig(({ command }) => {
   return {
@@ -25,6 +26,8 @@ export default defineConfig(({ command }) => {
       },
       outDir: '../dist',
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    plugins: [injectHTML(), FullReload(['./src/**/**.html']), webfontDownload([
+      'https://fonts.googleapis.com/css2?family=Inter+Tight:wght@100..900&display=swap'
+    ])],
   };
 });
